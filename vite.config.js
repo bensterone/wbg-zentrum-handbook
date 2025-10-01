d85ced7708ea1c5b07db5a501776e401f75fc1fe
bensterone <157856243+bensterone@users.noreply.github.com>
2025-10-01 19:53:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/wbg-zentrum-handbook/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     include: ['bpmn-js/lib/Modeler', 'bpmn-js/lib/Viewer']
