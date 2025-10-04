d779329a36071f214fa70f16fd1f9e37f3de9f28
bensterone <157856243+bensterone@users.noreply.github.com>
2025-10-04 18:47:08 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './tailwind.css';
+import './styles/tailwind.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
