af7ef32dc9611782014bae2b2046002cf40a1dec
bensterone <157856243+bensterone@users.noreply.github.com>
2025-10-07 21:16:21 +0200

    Update ProcessView.jsx

--- src/components/ProcessView.jsx
+++ src/components/ProcessView.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+
+const ProcessView = ({ item, isDarkMode }) => {
+  return (
+    <div>
+      <h2>Prozess Ansicht</h2>
+      <pre>{JSON.stringify(item, null, 2)}</pre>
+      {/* Hier kannst du später die echte Prozess-Anzeige implementieren */}
+    </div>
+  );
+};
+
+export default ProcessView;
 
