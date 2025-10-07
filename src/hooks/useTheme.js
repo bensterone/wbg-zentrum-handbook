226b121185c4543fb2b5881708a40a3c0e8ca912
bensterone <157856243+bensterone@users.noreply.github.com>
2025-10-07 22:23:14 +0200

    Update useTheme.js

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,5 +1,5 @@
+import React, { useState, useCallback, useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
-import { useEffect } from 'react';
 
 // Theme hook
 const useTheme = () => {
