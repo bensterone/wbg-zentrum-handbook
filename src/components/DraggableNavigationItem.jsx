f903ca1ed97ac423a7a15546e53bef6ea17bb241
bensterone <157856243+bensterone@users.noreply.github.com>
2025-10-07 23:35:13 +0200

    Update DraggableNavigationItem.jsx

--- src/components/DraggableNavigationItem.jsx
+++ src/components/DraggableNavigationItem.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useState, useCallback, useEffect } from 'react';
+import React, { memo, useState, useCallback, useEffect, useRef } from 'react';
 
 // Draggable Navigation Item Component
 const DraggableNavigationItem = memo(({ 
