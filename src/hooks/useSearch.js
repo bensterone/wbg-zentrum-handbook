627cfd4f755f3c58d2a66ba4b511c6bcadeea316
bensterone <157856243+bensterone@users.noreply.github.com>
2025-10-07 22:29:54 +0200

    Update useSearch.js

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,3 +1,4 @@
+import React, { useState, useCallback, useEffect } from 'react';
 // Enhanced search hook
 const useSearch = (data, searchFields = ['label', 'description']) => {
   const [query, setQuery] = useState('');
